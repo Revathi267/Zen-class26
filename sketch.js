341ddaf1620c0e40ff3cb826cd87d0ff71ee988f
Revathi267 <revathichezhian267@gmail.com>
2021-01-28 23:55:56 +0530

    added a box

--- sketch.js
+++ sketch.js
@@ -35,6 +35,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    box6 = new Box(100,240,70,70);
+
 }
 
 function draw(){
@@ -59,4 +61,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    box6.display();
 }
